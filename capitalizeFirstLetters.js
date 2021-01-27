6a9c1952104d6b2644d102c2b7e02cbc1eaf4112
Hotaru24 <florent-pasquet@protonmail.com>
2021-01-27 11:10:05 +0100

    tdd1 is completed

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+const capitalizeFirstLetters = (input) => {
+  return input.length > 0 ?
+  input.split(' ').map(e => e.charAt(0).toUpperCase() + e.slice(1)).join(' ')
+  : '';
+};
+
+// Check that capitalizeFirstLetters transforms multy words correctly
+assert.strictEqual(capitalizeFirstLetters('javaScript top tip'), 'JavaScript Top Tip');
+
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirstLetters(''), '');
+
+  
+module.exports = capitalizeFirstLetters;
